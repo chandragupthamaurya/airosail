29410320dd351ab501d2e1aef75e48ed564b7a5d
chandra <chandragupthamaurya128@gmail.com>
2021-07-22 06:05:06 -0700

    change the static

--- reposts/settings.py
+++ reposts/settings.py
@@ -147,7 +147,7 @@ LOGIN_URL = 'feed:index'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
-STATICFILES_DIRS = (os.path.join('static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
