11a3acd508f6c13c7ae6c76151a1fcc20535ecdf
chandra <chandragupthamaurya128@gmail.com>
2021-07-24 00:21:23 -0700

    add env

--- reposts/settings.py
+++ reposts/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY",get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.getenv("DEBUG",'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS","127.0.0.1,localhost,spaceairo.com").split(",")
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS","127.0.0.1,localhost,spaceairo.com").split(",")
 
 # Application definition
 
