28b946c553d6507fbbfd7ed1914fbd2d33cf8c07
chandra <chandragupthamaurya128@gmail.com>
2021-07-24 02:44:13 -0700

    change the settings

--- reposts/settings.py
+++ reposts/settings.py
@@ -14,10 +14,6 @@ from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
 from urllib.parse import urlparse
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY",get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.getenv("DEBUG",'False') == 'True'
 
-ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS","127.0.0.1,localhost,spaceairo.com").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS","127.0.0.1,localhost,spaceairo.com").split(",")
 
 # Application definition
 
@@ -160,8 +156,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
